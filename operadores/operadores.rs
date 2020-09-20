9754b2451ad29df2f06feeda7f2adbbe258e5138
AlexisBalderas27 <65316567+AlexisBalderas27@users.noreply.github.com>
2020-09-20 13:56:54 -0500

    Add files via upload

---
+++ operadores/operadores.rs
@@ -0,0 +1,66 @@
+/*
+Operadores en rust 
+name and surname Alexis Antonio Balderas Aguirre
+major: 160236 
+speciality professor: Juan Carlos González Ibarra
+institution: Universidad Politécnica de SLP
+enrollment: ITI
+
+*/
+    fn main(){
+        //se llaman a las funciones
+        funcionOr();
+        println!("---------------");
+        funcionAnd();
+        println!("---------------");
+        funcionNot();
+        println!("---------------");
+        funcionY();
+        
+    }
+    
+    //en esta funcion se comprobara el operador logico or
+    fn funcionOr(){
+        let booleanos = [false, true]; //este es el arreglo que se define los valores booleanos mas no strings
+         println!("x\ty\tx or y");
+         for x in &booleanos{ //se recorre el arreglo con dos foreach
+               for y in &booleanos{
+                    print!("{}",x);  print!("\t{}", y);  println!("\t{}\n",x|y); /*el operador logico | (or) informara al programa si alguno 
+                     es verdadero y la salida sera verdadero*/
+               }
+                
+         }
+    }
+    //en esta funcion se comprobara el operador logico and
+    fn funcionAnd(){
+        let booleanos = [false, true]; //este es el arreglo que se define los valores booleanos mas no strings
+         println!("x\ty\tx and y");
+         for x in &booleanos{
+               for y in &booleanos{
+                    print!("{}",x);  print!("\t{}", y);  println!("\t{}\n",x&y); /*el operador logico & (and) informara al programa si ambos 
+                    son verdadero y la salida sera verdadero*/
+               }
+                
+         }
+    }
+    //en esta funcion se comprobara el operador logico not
+    fn funcionNot(){
+        let booleanos = [false, true]; //este es el arreglo que se define los valores booleanos mas no strings
+         println!("x\tnot x");
+         for x in &booleanos{
+             print!("{}",x);  println!("\t{}\n",!x); /*el operador logico ! (not) informara al programa si alguno 
+             es verdadero y la salida sera verdadero*/
+         }
+    }
+    
+    //en esta funcion se comprobara el operador logico 
+    fn funcionY(){
+        let booleanos = [false, true]; //este es el arreglo que se define los valores booleanos mas no strings
+         println!("x\ty\tx and y");
+         for x in &booleanos{
+               for y in &booleanos{
+                    print!("{}",x);  print!("\t{}", y);  println!("\t{}\n",x^y);
+               }
+                
+         }
+    }
