e15c50cb4d2923c5fd81704911311c75d36d92b3
AlexisBalderas27 <65316567+AlexisBalderas27@users.noreply.github.com>
2020-10-06 21:34:19 -0500

    Update afd.rs

--- afd/afd.rs
+++ afd/afd.rs
@@ -7,30 +7,36 @@ institution: Universidad Politécnica de SLP
 enrollment: ITI
 */
 
+
+use std::process;
+use std::io;
+use std::str;
+
 //Definimos la funcion caracter 
-fn caracter(character: char) -> char{
-    let simbolo="";
-    let Fin=' ';
-    let digito=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+fn caracter(character: char) -> i32{
+    let mut Fin = " ";
     let operador=["+","-","*","/"];
     
     //#comparamos si es digito o operador
-    if character == '0' || character == '1' || character == '2' || character == '3' || character == '4' || character == '5' || character == '6' || character == '7' || character == '8' || character == '9' {
-        simbolo=" Digito ";
-        return '0';
-        }
+    let mut s = String::from("");
+	s.push(character);
+    let newCharacter: &str = &s[..];
+    
+    if character.is_digit(10){
+			return 0;                   //  Si es digito devuelve un cero          
+    }
     else{
         if character == '+' || character == '-' || character == '*' || character == '/' {
-            simbolo="Operador";
-            return '1';
+            return 1;
             }
         else{
-            if character==Fin {
-                return '2';
+            if newCharacter==Fin {
+                return 2;
                 }
         }
         //#si no es ni un digito ni un operador entonces es un caracter no validp
         println!("Error el caracter: {:?}",character); print!("no es valido");
+        process::exit(0x0100);
         }
 }
 
@@ -40,8 +46,8 @@ fn encabezado(){
     }
 
 //definimos la funcion contenido donde guarda cada valor despues de encontrarlo en un ciclo
-fn contenido(estadosig: char,character: char,simbolo :char,estado: char){
-    //println!("|     ",estadosig,"      |  ",character,"    |",simbolo," |     ",estado,"       |")
+ fn contenido(estadosig: i32, character: char, simbolo: &str, estado: i32){
+     println! ("|      {}       |    {}    |  {} |      {}        |",estadosig,character,simbolo,estado);
     body();
 }
 
@@ -52,47 +58,63 @@ fn body(){
 
 
 fn main(){
-//Este es la tabla de transiciones del automata AFD creado
-    let tabla=[["1","E","E"],["E","2","E"],["3","E","E"],["E","E","A"]];
-    let estado = "0";
-    let mut cadena;
+    //let tabla=[["1","E","E"],["E","2","E"],["3","E","E"],["E","E","A"]]; tabla original
+    let tabla = [[1,5,5], [5,2,5], [3,5,5], [5,5,0]];  //  se cambian los valores de A y E por 0 y 5
+    let mut  tipo: String = "".to_string(); 
+    let mut  Fin: String = "".to_string();  
+    let mut estado = 0;      
+            
+
+    let mut estado = 0;
+    let mut cadena= String::new();	
     let mut estadosig;
     println!("+-------------------------------------+
     |    Ingrese una cadena a evaluar:    |
     +-------------------------------------+");
-    //cadena = input();
+    io::stdin().read_line(&mut cadena);
     body();
     encabezado();
 
     //#ciclo para recorrer la cadena
-    for  character in cadena{
-        estadosig=estado;
+    for  character in cadena.chars() {
+        estadosig = estado;
         
+        //  Llamamos al metodo para saber si es un caracter valido y el valor retornado se guarda en charcaracter
+        let mut charcaracter: i32 = caracter(character);
         //#llamamos al metodo para saber si es un caracter valido y el valor retornado se guarda en charcaracter
         let charcaracter= caracter(character);
+        if charcaracter == 0{                   //  Si es digito la variable tipo guarda "Digito"
+      	    tipo = "Digito".to_string();
+      	}
+      	else if charcaracter == 1{              //  Si es operador la variable tipo guarda "Operador"
+      	    tipo = "Operador".to_string();
+      	}
+      	else if charcaracter == 2{              //  Si es vacio la variable tipo guarda "Vacio"
+      	    tipo = "Error".to_string();
+      	}
         
         //#guardamos en estado el valor obtenido en la tabla segun las cordenadas que recibio anteriormente
-        estado=tabla[estado][charcaracter];
+        estado = tabla[estado as usize][charcaracter as usize];
     
       
         //#Si el valor obtenido es una E imprimimos cadena no valida
-        if  estado=="E" {
-            //println!("|     ",estadosig,"      |  ",character,"    |",simbolo," |     ",estado,"       |");
+         if estado == 69{                        //  Si el estado en el que estamos es el E significa que la cadena no es valida
+            println!("|    {}    |  {}    |     {}     |     {}      |", estadosig, character, tipo, estado);
             body();
-            println!("|              Cadena No Valida :(                   |
+            println!("|              Cadena No Valida                       |
     +----------------------------------------------------+");
-            }
-        //contenido(estadosig,character,simbolo,estado);
+            process::exit(0x0100);
+        }
     }
 
     //#al concluir si el estado no es 3 que es el de aceptacion imprimimos cadena no valida    
-    if estado!="3" {
+    if estado!=3 {
             println!("|              Cadena No Valida :(                   |
     +----------------------------------------------------+");
     }
     //si el estado es 3 es una cadena de aceptacion
-    if estado=="3" {
-        //println!("|     ",estado,"      |         |Fin Cadena |               |");
+    if estado==3 {
+        println! ("|      {}       |         |Fin Cadena |               |",estado);
         body();
         println!("|                Cadena Valida                       |
     +----------------------------------------------------+");
